552eb547f3e100701be0bd2d904aa194d6583004
Subhi Dweik <subhime@gmail.com>
2016-02-04 21:15:41 +0100

    Started cleaning up dataconnector

--- bin/connector_format.rb
+++ bin/connector_format.rb
@@ -1,85 +1,110 @@
 require 'savon'
 require 'pp'
-require 'pry'
 require 'optparse'
-	def from_xml(xml_io)
-		begin
-			result = Nokogiri::XML(xml_io)
-			return { result.root.name.to_sym => xml_node_to_hash(result.root)}
-		rescue Exception => e
-			# raise your custom exception here
-		end
+
+# Parse xml to a hash
+def from_xml(xml_io)
+	begin
+		result = Nokogiri::XML(xml_io)
+		return { result.root.name.to_sym => xml_node_to_hash(result.root)}
+	rescue Exception => e
+		puts e.backtrace
+		raise
 	end
+end
 
-	def xml_node_to_hash(node)
-		# If we are at the root of the document, start the hash
-		if node.element?
-			result_hash = {}
-			if node.attributes != {}
-				attributes = {}
-				node.attributes.keys.each do |key|
-					attributes[node.attributes[key].name.to_sym] = node.attributes[key].value
-				end
+# Parse each node to a hash
+def xml_node_to_hash(node)
+	# If we are at the root of the document, start the hash
+	if node.element?
+		result_hash = {}
+		if node.attributes != {}
+			attributes = {}
+			node.attributes.keys.each do |key|
+				attributes[node.attributes[key].name.to_sym] = node.attributes[key].value
 			end
-			if node.children.size > 0
-				node.children.each do |child|
-					result = xml_node_to_hash(child)
+		end
+		if node.children.size > 0
+			node.children.each do |child|
+				result = xml_node_to_hash(child)
 
-					if child.name == "text"
-						unless child.next_sibling || child.previous_sibling
-							return result unless attributes
-							result_hash[child.name.to_sym] = result
-						end
-					elsif result_hash[child.name.to_sym]
-
-						if result_hash[child.name.to_sym].is_a?(Object::Array)
-							result_hash[child.name.to_sym] << result
-						else
-							result_hash[child.name.to_sym] = [result_hash[child.name.to_sym]] << result
-						end
-					else
+				if child.name == "text"
+					unless child.next_sibling || child.previous_sibling
+						return result unless attributes
 						result_hash[child.name.to_sym] = result
 					end
+				elsif result_hash[child.name.to_sym]
+
+					if result_hash[child.name.to_sym].is_a?(Object::Array)
+						result_hash[child.name.to_sym] << result
+					else
+						result_hash[child.name.to_sym] = [result_hash[child.name.to_sym]] << result
+					end
+				else
+					result_hash[child.name.to_sym] = result
 				end
-				if attributes
-					#add code to remove non-data attributes e.g. xml schema, namespace here
-					#if there is a collision then node content supersets attributes
-					result_hash = attributes.merge(result_hash)
-				end
-				return result_hash
-			else
-				return attributes
 			end
+			if attributes
+				#add code to remove non-data attributes e.g. xml schema, namespace here
+				#if there is a collision then node content supersets attributes
+				result_hash = attributes.merge(result_hash)
+			end
+			return result_hash
 		else
-			return node.content.to_s
+			return attributes
 		end
+	else
+		return node.content.to_s
 	end
+end
 
 
+# The dataconnector url, will probably not need to change
 dataconnector_url = 'https://profitweb.afasonline.nl/profitservices/DataConnector.asmx?WSDL'
+
+# The username to use
 username = nil
+
+# The password to use
 password = nil
+
+# The connector we want a description of
 connector_name = nil
+
+# The AFAS environment, probably in the format: 'X12345XX'
 environment_id = nil
 
+# Create an optionparser
 OptionParser.new do |opts|
+	# Set username
 	opts.on('-u', '--username NAME', 'AFAS username') { |v| username = v }
+
+	# Set password
 	opts.on('-p', '--password PASSWORD', 'AFAS password') { |v| password = v }
+
+	# Set connector name
 	opts.on('-c', '--connector CONNECTOR', 'Name of the UpdateConnector') { |v| connector_name = v }
+
+	# Set environment
 	opts.on('-e', '--environment ENVIRONMENT', 'Name of the environment') { |v| environment_id = v }
+
+	# Display help
 	opts.on_tail("-h", "--help", "Show this message") do
 		puts opts
 		exit
 	end
+	# Parse
 end.parse!
 
+# All params are required, fail is one is missing
 fail ArgumentError.new('Username is required') unless username
 fail ArgumentError.new('Password is required') unless password
 fail ArgumentError.new('Connector name is required') unless connector_name
 fail ArgumentError.new('Environment name is required') unless environment_id
 
+# Setup Savon with the credentials the user supplied
 client = Savon.client(wsdl: dataconnector_url, basic_auth: [username, password])
-x = client.call(:execute, {
+resp = client.call(:execute, {
 	message: {
 		environmentId: environment_id,
 		userId: username,
@@ -89,9 +114,12 @@ x = client.call(:execute, {
 	}
 })
 
-xml = x.hash[:envelope][:body][:execute_response][:execute_result]
-y = from_xml(xml)[:AfasDataConnector][:ConnectorData][:Schema]
-a = Nokogiri::XML(y).remove_namespaces!
+# Get response xml out of wrapper
+xml = resp.hash[:envelope][:body][:execute_response][:execute_result]
+
+
+schemaxml = from_xml(xml)[:AfasDataConnector][:ConnectorData][:Schema]
+a = Nokogiri::XML(schemaxml).remove_namespaces!
 
 main_object_xpath = '//*[@name="Fields" and not(ancestor::*[@name="Objects"])]'
 nested_objects_xpath = '//*[@name="Objects"]/complexType/sequence/*'
